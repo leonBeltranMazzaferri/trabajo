3d02fb246d2825422b65e9c9478f82d589725bb3
leonbeltranmazzaferri <leonbeltranm07@gmail.com>
2025-04-26 02:45:09 -0300

    b

---
+++ js/nodulos.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementById("header").innerHTML = "<h1> OLyMPUS GYM</h1> <p>Entrena como un dios</p> ";
+
+    document.getElementById("nav").innerHTML = `
+        <ul>
+            <li><a href='index.html'>Suplementos</a></li>
+            <li><a href='pagina1.html'>Indumentaria</a></li>
+            <li><a href='pagina2.html'>Equipamientp</a></li>
+        </ul>`;
+
+    document.getElementById("footer").innerHTML = "<p> INTEGRANTES : LEON BELTRAN - BELEN MARTINEZ - BRISA FARIAS </p>";
+});
