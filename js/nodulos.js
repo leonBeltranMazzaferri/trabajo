b01388ac42f4c94691541d44107d7613e34f346f
leonbeltranmazzaferri <leonbeltranm07@gmail.com>
2025-04-28 17:12:36 -0300

    kjdbvbdj

--- js/nodulos.js
+++ js/nodulos.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    document.getElementById("header").innerHTML = "<h1> OLyMPUS GYM</h1> <p>Entrena como un dios</p> ";
+    document.getElementById("header").innerHTML = " <input type='checkbox' id='menu'></input> <label for='menu' class=' lavel ' > <img src='/images/icono/menu-alt-left-regular-24 (1).ico' class='img-menu' ></label> <i class='bx bx-search-alt' id='buscador'></i>  <h1> OLyMPUS GYM</h1> <i class='bx bxs-user'></i> <i class='bx bx-cart'></i> ";
 
     document.getElementById("nav").innerHTML = `
-        <ul>
-            <li><a href='index.html'>Suplementos</a></li>
-            <li><a href='pagina1.html'>Indumentaria</a></li>
-            <li><a href='pagina2.html'>Equipamientp</a></li>
+        <ul class='menu'>
+            <li><a href='index.html'>Suplementos</a> <i class='bx bxs-zap'></i> </li>
+            <li><a href='pagina1.html'>Indumentaria</a> <i class='bx bxs-star'></i> </li>
+            <li><a href='pagina2.html'>Equipamiento</a> <i class='bx bx-dumbbell'></i> </li>
         </ul>`;
 
     document.getElementById("footer").innerHTML = "<p> INTEGRANTES : LEON BELTRAN - BELEN MARTINEZ - BRISA FARIAS </p>";
