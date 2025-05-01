d9dbebba1f34e3c5b2ed08b3b916fd4d7ba7a454
leonbeltranmazzaferri <leonbeltranm07@gmail.com>
2025-05-01 01:33:56 -0300

    tercera pagina y catalogo

--- js/nodulos.js
+++ js/nodulos.js
@@ -6,7 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
     <input type='checkbox' id='menu-hamburguesa'>
     </input> <label for='menu-hamburguesa' class='lavel' > <img src='/images/icono/menu-alt-left-regular-24 (1).ico' class='img-menu' ></label>
         <ul class='menu'>
-            <li><a href='index.html'>Suplementos</a> <i class='bx bxs-zap'></i> </li>
+            <li><a href='index.html'>inicio</a> <i class='bx bxs-home'></i> </li>
+            <li><a href='pagina3.html'>suplementos</a> <i class='bx bxs-zap'></i> </li>
             <li><a href='pagina1.html'>Indumentaria</a> <i class='bx bxs-star'></i> </li>
             <li><a href='pagina2.html'>Equipamiento</a> <i class='bx bx-dumbbell'></i> </li>
         </ul>`;
