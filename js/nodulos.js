e6ad766b1b78b4c8e1a3fdf4a512ac045dcff360
leonbeltranmazzaferri <leonbeltranm07@gmail.com>
2025-05-12 17:51:49 -0300

    correcciones que  mandaste a hacer a ultimo momento como la profecional que sos

--- js/nodulos.js
+++ js/nodulos.js
@@ -7,9 +7,8 @@ document.addEventListener("DOMContentLoaded", function() {
     </input> <label for='menu-hamburguesa' class='lavel' > <img src='images/icono/menu-alt-left-regular-24 (1).ico' class='img-menu' ></label>
         <ul class='menu'>
             <li><a href='index.html'>inicio</a> <i class='bx bxs-home'></i> </li>
-            <li><a href='pagina3.html'>suplementos</a> <i class='bx bxs-zap'></i> </li>
-            <li><a href='pagina1.html'>Indumentaria</a> <i class='bx bxs-star'></i> </li>
-            <li><a href='pagina2.html'>Equipamiento</a> <i class='bx bx-dumbbell'></i> </li>
+            <li><a href='indumentaria.html'>Indumentaria</a> <i class='bx bxs-star'></i> </li>
+            <li><a href='contacto.html'>contactanos</a> <i class='bx bx-dumbbell'></i> </li>
         </ul>`;
 
     document.getElementById("footer").innerHTML = "<p> INTEGRANTES : LEON BELTRAN - BELEN MARTINEZ  </p>";
